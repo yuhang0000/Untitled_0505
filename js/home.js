07d67bf7c0a76bf5587db6c911741c20a2ba40ab
yuhang0000 <104742794+yuhang0000@users.noreply.github.com>
2024-10-05 23:37:17 +0800

    Add files via upload

--- js/home.js
+++ js/home.js
@@ -5,4 +5,33 @@ document.addEventListener('DOMContentLoaded', function() {
             alert('你還沒做 (╯▔皿▔)╯');
         });
     });
+    var BG1 = document.getElementById('BG1');
+    let BG1X = 0;
+
+// 创建一个延迟函数
+function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+// 使用 async/await 实现延时循环
+(async function() {
+    while (true) {
+        BG1X--;
+        console.log(BG1X);
+
+        // 如果需要停止循环，可以在这里添加条件判断
+        if (BG1X > 0) { // 例如，当 BG1X 达到 10 时停止
+            break;
+        }
+        
+        if (BG1X == -64){
+          BG1X = 0;
+        }
+        
+        BG1.style.backgroundPosition = "0 " + BG1X + "px";
+
+        await delay(20); // 延迟 200ms
+    }
+})();
+
 });
