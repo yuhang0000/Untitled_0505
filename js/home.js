b1cccbedb8252c1152fbfab893f14d11dd65f23d
yuhang0000 <104742794+yuhang0000@users.noreply.github.com>
2024-10-06 01:49:08 +0800

    Add files via upload

--- js/home.js
+++ js/home.js
@@ -8,30 +8,25 @@ document.addEventListener('DOMContentLoaded', function() {
     var BG1 = document.getElementById('BG1');
     let BG1X = 0;
 
-// 创建一个延迟函数
-function delay(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-// 使用 async/await 实现延时循环
-(async function() {
-    while (true) {
-        BG1X--;
-        console.log(BG1X);
-
-        // 如果需要停止循环，可以在这里添加条件判断
-        if (BG1X > 0) { // 例如，当 BG1X 达到 10 时停止
-            break;
-        }
-        
-        if (BG1X == -64){
-          BG1X = 0;
-        }
-        
-        BG1.style.backgroundPosition = "0 " + BG1X + "px";
-
-        await delay(20); // 延迟 200ms
+    //这是延时
+    function delay(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
     }
-})();
+
+    //背景滚动
+    (async function() {
+        while (null == null) {
+            BG1X--;
+            console.log(BG1X);
+            if (BG1X > 0) {
+                break;
+            }
+            if (BG1X == -64){
+              BG1X = 0;
+            }
+            BG1.style.backgroundPosition = "0 " + BG1X + "px";
+            await delay(100);
+        }
+    })();
 
 });
